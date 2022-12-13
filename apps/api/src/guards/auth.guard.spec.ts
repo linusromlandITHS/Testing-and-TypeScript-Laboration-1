d501a07e5306d19aa3bed1d38e3e7c33d80acf02
Linus Romland <hello@linusromland.com>
2022-12-13 14:08:59 +0100

    Moved getAccessToken to helper function

--- apps/api/src/guards/auth.guard.spec.ts
+++ apps/api/src/guards/auth.guard.spec.ts
@@ -1,17 +1,14 @@
 // External dependencies
 import { ExecutionContext } from '@nestjs/common';
 import { createMock } from '@golevelup/ts-jest';
-import { AxiosResponse } from '@nestjs/terminus/dist/health-indicator/http/axios.interfaces';
-import { HttpService } from '@nestjs/axios';
-import { Observable, map, lastValueFrom } from 'rxjs';
 
 // Internal dependencies
 import { AuthGuard } from './auth.guard';
+import getAccessToken from '$src/../test/helpers/getAccessToken.helper';
+
 describe('AuthGuard', () => {
 	let authGuard: AuthGuard;
 	let mockContext: any;
-
-	const { AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_TEST_USERNAME, AUTH0_TEST_PASSWORD } = process.env;
 
 	beforeEach(async () => {
 		authGuard = new AuthGuard();
@@ -39,30 +36,11 @@ describe('AuthGuard', () => {
 	});
 
 	it('should return true since token is valid', async () => {
-		const httpService: HttpService = new HttpService();
-
-		type Auth0M2MObject = {
-			access_token: string;
-			expires_in: number;
-			token_type: string;
-		};
-
-		const observable: Observable<Auth0M2MObject> = httpService
-			.post(`https://${AUTH0_DOMAIN}/oauth/token`, {
-				username: AUTH0_TEST_USERNAME,
-				password: AUTH0_TEST_PASSWORD,
-				client_id: AUTH0_CLIENT_ID,
-				client_secret: AUTH0_CLIENT_SECRET,
-				audience: `https://${AUTH0_DOMAIN}/api/v2/`,
-				grant_type: 'password'
-			})
-			.pipe(map((response: AxiosResponse) => response.data));
-
-		const data: Auth0M2MObject = await lastValueFrom(observable);
+		const accessToken: string = await getAccessToken();
 
 		mockContext.switchToHttp().getRequest.mockReturnValue({
 			headers: {
-				authorization: `Bearer ${data.access_token}`
+				authorization: `Bearer ${accessToken}`
 			}
 		});
 
---
+++ apps/api/test/helpers/getAccessToken.helper.ts
@@ -0,0 +1,30 @@
+// External dependencies
+import { HttpService } from '@nestjs/axios';
+import { Observable, map, lastValueFrom } from 'rxjs';
+import { AxiosResponse } from '@nestjs/terminus/dist/health-indicator/http/axios.interfaces';
+
+export default async function getAccessToken(): Promise<string> {
+	const { AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_TEST_USERNAME, AUTH0_TEST_PASSWORD } = process.env;
+
+	const httpService: HttpService = new HttpService();
+
+	type Auth0M2MObject = {
+		access_token: string;
+		expires_in: number;
+		token_type: string;
+	};
+
+	const observable: Observable<Auth0M2MObject> = httpService
+		.post(`https://${AUTH0_DOMAIN}/oauth/token`, {
+			username: AUTH0_TEST_USERNAME,
+			password: AUTH0_TEST_PASSWORD,
+			client_id: AUTH0_CLIENT_ID,
+			client_secret: AUTH0_CLIENT_SECRET,
+			audience: `https://${AUTH0_DOMAIN}/api/v2/`,
+			grant_type: 'password'
+		})
+		.pipe(map((response: AxiosResponse) => response.data));
+
+	const data: Auth0M2MObject = await lastValueFrom(observable);
+	return data.access_token;
+}
