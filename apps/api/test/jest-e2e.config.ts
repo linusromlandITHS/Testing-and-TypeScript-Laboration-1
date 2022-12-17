7da0e3dc4c3512e639dd4e5882934fe406990844
Linus Romland <hello@linusromland.com>
2022-12-17 14:22:50 +0100

    Improved e2e tests

---
+++ apps/api/test/jest-e2e.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from '../tsconfig.json';
+
+const config: Config = {
+	moduleFileExtensions: ['js', 'json', 'ts'],
+	rootDir: '.',
+	testEnvironment: 'node',
+	testRegex: '.e2e-spec.ts$',
+	transform: {
+		'^.+\\.(t|j)s$': 'ts-jest'
+	},
+	globalSetup: '<rootDir>/helpers/setup.helper.ts',
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/../' })
+};
+
+export default config;
