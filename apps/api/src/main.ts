f7a753c684f2f127c81aa1e4c024b5151aff74fa
Linus Romland <hello@linusromland.com>
2022-12-20 09:40:26 +0100

    Switched to express platform for nest

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,17 +1,13 @@
 // External dependencies
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { INestApplication } from '@nestjs/common';
 
 // Internal dependencies
 import { AppModule } from './app.module';
 import { PORT } from '$src/utils/env';
 
 async function bootstrap(): Promise<void> {
-	const app: NestFastifyApplication = await NestFactory.create<NestFastifyApplication>(
-		AppModule,
-		new FastifyAdapter(),
-		{ cors: true }
-	);
+	const app: INestApplication = await NestFactory.create(AppModule, { cors: true });
 
 	await app.listen(PORT);
 }
