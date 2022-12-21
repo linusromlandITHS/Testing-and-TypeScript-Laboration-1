8258a77be51233b47fb80aa5a20395f5a201907f
Linus Romland <hello@linusromland.com>
2022-12-21 17:25:10 +0100

    Updated default API_URL

--- apps/client/src/utils/env.ts
+++ apps/client/src/utils/env.ts
@@ -1,5 +1,5 @@
 const AUTH0_DOMAIN: string = import.meta.env.PUB_AUTH0_DOMAIN;
 const AUTH0_CLIENT_ID: string = import.meta.env.PUB_AUTH0_CLIENT_ID;
-const API_URL: string = import.meta.env.PUB_API_URL || 'http://localhost:3001';
+const API_URL: string = import.meta.env.PUB_API_URL || 'http://127.0.0.1:3001';
 
 export { AUTH0_DOMAIN, AUTH0_CLIENT_ID, API_URL };
